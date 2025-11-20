46001366ce6475ebaaaeef6415d8e2ea7c86c5f7
ManoelMessias32 <manoeletrico2hotmail.com@gmail.com>
2025-11-20 00:46:02 -0300

    Build: Adiciona configuração do Vite para aumentar limite de chunk

---
+++ frontend/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, // Aumenta o limite para 1600 kB
+  },
+});
