3411e32edbe5ff5d81e8cb610a04bf4d81fca67b
ManoelMessias32 <manoeletrico2hotmail.com@gmail.com>
2025-11-25 11:02:14 -0300

    fix: ajuste da tela e correçao de  joos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <<< ADICIONADO PARA CORRIGIR CAMINHOS NO DEPLOY
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600, // Aumenta o limite para 1600 kB
