8117b8bb5a156d3fe309d36118f093e9cfa7f4ec
ManoelMessias32 <manoeletrico2hotmail.com@gmail.com>
2025-11-27 16:49:26 -0300

    Fix images +candy crash tela

--- frontend/build/games/candy-crush/script.js
+++ frontend/build/games/candy-crush/script.js
@@ -21,13 +21,14 @@ function candyCrushGame() {
     let gameInterval = null;
     let timerInterval = null;
 
+    const base = "https://cryptodesktop.vercel.app/utils/";
     const candyColors = [
-        "url(https://raw.githubusercontent.com/arpit456jain/Amazing-Js-Projects/master/Candy%20Crush/utils/red-candy.png)",
-        "url(https://raw.githubusercontent.com/arpit456jain/Amazing-Js-Projects/master/Candy%20Crush/utils/blue-candy.png)",
-        "url(https://raw.githubusercontent.com/arpit456jain/Amazing-Js-Projects/master/Candy%20Crush/utils/green-candy.png)",
-        "url(https://raw.githubusercontent.com/arpit456jain/Amazing-Js-Projects/master/Candy%20Crush/utils/yellow-candy.png)",
-        "url(https://raw.githubusercontent.com/arpit456jain/Amazing-Js-Projects/master/Candy%20Crush/utils/orange-candy.png)",
-        "url(https://raw.githubusercontent.com/arpit456jain/Amazing-Js-Projects/master/Candy%20Crush/utils/purple-candy.png)",
+        `url(${base}red-candy.png)`,
+        `url(${base}blue-candy.png)`,
+        `url(${base}green-candy.png)`,
+        `url(${base}yellow-candy.png)`,
+        `url(${base}orange-candy.png)`,
+        `url(${base}purple-candy.png)`
     ];
 
     // Create the Game Board
